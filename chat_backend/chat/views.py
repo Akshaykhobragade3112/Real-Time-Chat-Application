9db986df0274d10a7a53cbe13b7c0013eca9f6c5
Akshaykhobragade3112 <akshaykhobragade40@gmail.com>
2025-09-26 12:14:19 +0530

    add: new feature delete room only by creator

--- chat_backend/chat/views.py
+++ chat_backend/chat/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from .models import ChatRoom, Message
 from .serializers import ChatRoomSerializer, MessageSerializer
 from rest_framework.response import Response
-from rest_framework import status
 
 
 class ChatRoomListCreateView(generics.ListCreateAPIView):
     queryset = ChatRoom.objects.all()
     serializer_class = ChatRoomSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
 
 class ChatRoomDeleteView(generics.DestroyAPIView):
     queryset = ChatRoom.objects.all()
@@ -22,6 +24,14 @@ class ChatRoomDeleteView(generics.DestroyAPIView):
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
+
+        # Restrict deletion to the creator
+        if instance.creator != request.user:
+            return Response(
+                {"error": "You are not allowed to delete this room."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        
         room_name = instance.name
         self.perform_destroy(instance)
         return Response(
