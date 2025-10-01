67190be3282e9555c0713934217241b61baebdb5
Akshaykhobragade3112 <akshaykhobragade40@gmail.com>
2025-10-01 21:26:43 +0530

    intil commit

--- chat_frontend/src/api/api.js
+++ chat_frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/", 
-  // baseURL: "/api/",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
+  withCredentials: true,
 });
 
 API.interceptors.request.use((config) => {
