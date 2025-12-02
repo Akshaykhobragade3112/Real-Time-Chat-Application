a9bd459c2cc9ada51b0c85fa85094add89532280
Akshaykhobragade3112 <akshaykhobragade40@gmail.com>
2025-12-02 12:00:34 +0530

    change api.js file for deploy

--- chat_frontend/src/api/api.js
+++ chat_frontend/src/api/api.js
@@ -1,15 +1,15 @@
+// src/api/api.js
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL ?? "http://127.0.0.1:8000/api/";
+
 const API = axios.create({
-  // baseURL: "http://127.0.0.1:8000/api/", 
-  baseURL: "/api/",
+  baseURL,
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
